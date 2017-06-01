64052fbdcb230a14b4cc8132996bd6df296e0a4c
Rob <robe16@hotmail.co.uk>
2017-06-01 23:43:43 +0100

    Section added to index.html for messages to be populated in advance to modules to be written.

--- src/web/web_create_home.py
+++ src/web/web_create_home.py
@@ -4,7 +4,8 @@ from src.axiscare.carer_string import carer_string
 
 
 def create_home():
-    args = {'carer_msg': carer_string()}
+    args = {'carer_msg': carer_string(),
+            'messages': ''}
     #
     body = urlopen('html/index.html').read().encode('utf-8').format(**args)
     #
